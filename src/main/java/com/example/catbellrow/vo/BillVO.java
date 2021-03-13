e66a4c3753e8a02d3e164bc182d828f89d450d48
jacob <junghi1015@gmail.com>
2021-03-13 17:10:10 +0900

    법안 국회의원 VO 최초

---
+++ src/main/java/com/example/catbellrow/vo/BillVO.java
@@ -0,0 +1,24 @@
+package com.example.catbellrow.vo;
+
+/*
+ *
+ * title : 법안머시기 getter/setter
+ * author : 정효인
+ * date : 2021.03.13
+ *
+ * */
+
+import java.util.Date;
+
+public class BillVO {
+    private int bill_code;
+    private String bill_title;
+    private int bill_state;
+    private Date start_date;
+    private Date end_date;
+    private int bill_type;
+    private String bill_context;
+    private int like;
+    private int bill_mps;
+    private String bill_party;
+}
---
+++ src/main/java/com/example/catbellrow/vo/MPVO.java
@@ -0,0 +1,8 @@
+package com.example.catbellrow.vo;
+
+public class MPVO {
+    private int mp_code;
+    private String mp_name;
+    private String party;
+
+}
