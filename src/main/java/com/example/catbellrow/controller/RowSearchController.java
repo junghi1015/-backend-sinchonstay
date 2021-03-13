f2d7185d19ce1d11631c295467e64b41a5a6fec2
beom21c <beom21c@gmail.com>
2021-03-13 17:14:22 +0900

    first frame

--- src/main/java/com/example/catbellrow/controller/RowSearchController.java
+++ src/main/java/com/example/catbellrow/controller/RowSearchController.java
@@ -1,5 +1,7 @@
 package com.example.catbellrow.controller;
 
+import com.example.catbellrow.service.MemberOfCongressService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/simple")
 public class RowSearchController {
 
+    @Autowired
+    private MemberOfCongressService memberOfCongressService;
+
     @GetMapping(value = "/test", produces = {MediaType.APPLICATION_JSON_VALUE})
     public void simpleTest(){
-        System.out.println("test");
+        System.out.println(memberOfCongressService.getCongMember());
     }
 }
