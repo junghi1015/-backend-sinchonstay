fe45d15d48d0458b2dc9e6ed556c4cec85710689
jacob <junghi1015@gmail.com>
2021-03-13 16:25:20 +0900

    국회의원멤버 VO 수정중

---
+++ src/main/java/com/example/catbellrow/vo/MemberOfCongressVO.java
@@ -0,0 +1,31 @@
+package com.example.catbellrow.vo;
+
+public class MemberOfCongressVO {
+    private String ttts;
+    private int ggbs;
+    private String vvs;
+
+    public String getTtts() {
+        return ttts;
+    }
+
+    public void setTtts(String ttts) {
+        this.ttts = ttts;
+    }
+
+    public int getGgbs() {
+        return ggbs;
+    }
+
+    public void setGgbs(int ggbs) {
+        this.ggbs = ggbs;
+    }
+
+    public String getVvs() {
+        return vvs;
+    }
+
+    public void setVvs(String vvs) {
+        this.vvs = vvs;
+    }
+}
