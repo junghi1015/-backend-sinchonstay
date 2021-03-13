abe901ff71653e4d90be18895c5c96e03071cc9e
jacob <junghi1015@gmail.com>
2021-03-13 16:51:44 +0900

    국회의원멤버 VO 수정중

--- src/main/java/com/example/catbellrow/vo/MemberOfCongressVO.java
+++ src/main/java/com/example/catbellrow/vo/MemberOfCongressVO.java
@@ -1,11 +1,6 @@
 package com.example.catbellrow.vo;
 
 public class MemberOfCongressVO {
-    private String bill_code;
-    private String ;
-    private String ;
-    private String ;
-    private String ;
-    private String ;
+
 
 }
